01bb0e2ddc17c1a842fc78b0cb796cf70a369a94
DoLT <dolt@guide.inc>
2023-02-01 11:38:26 +0700

    chore: split command

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,17 +4,12 @@
 )]
 
 mod menu;
-
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from nnnRust!", name)
-}
+mod command;
 
 fn main() {
     tauri::Builder::default()
         .menu(menu::get_menu())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![command::greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
