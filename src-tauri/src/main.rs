023490f08123559665cd6c92294ae8a8c1d0008a
DoLT <dolt@guide.inc>
2023-02-01 10:30:50 +0700

    feat: add menu

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,25 +3,17 @@
     windows_subsystem = "windows"
 )]
 
+mod menu;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from nnnRust!", name)
 }
 
-use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
-
 fn main() {
-    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
-    let close = CustomMenuItem::new("close".to_string(), "Close");
-    let submenu = Submenu::new("File", Menu::new().add_item(quit).add_item(close));
-    let menu = Menu::new()
-    .add_native_item(MenuItem::Copy)
-    .add_item(CustomMenuItem::new("hide", "Hide"))
-    .add_submenu(submenu);
-    
     tauri::Builder::default()
-        .menu(menu)
+        .menu(menu::get_menu())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
